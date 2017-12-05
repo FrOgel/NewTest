965cb4a9abd53ff76ce2d2f97371193fe45d8794
FrOgel <31237261+FrOgel@users.noreply.github.com>
2017-12-05 10:27:13 +0100

    rest ekb

--- TestEJB/ejbModule/test/Hello.java
+++ TestEJB/ejbModule/test/Hello.java
@@ -1,11 +1,18 @@
 package test;
 
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Session Bean implementation class Hello
  */
 @Stateless
+@Remote
+@Path("/EJB")
 public class Hello implements HelloRemote {
 
     /**
@@ -13,6 +20,13 @@ public class Hello implements HelloRemote {
      */
     public Hello() {
         // TODO Auto-generated constructor stub
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("getEJB")
+    public String getEJBWelcome() {
+    	return "Hello from REST EJB";
     }
     
 	public String getWelcome() {
